6e31083e38779f1550d62ae16af5c9890fca006c
tariquesiddique <admin@example.com>
2016-10-31 01:00:59 +0600

    api 2

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from blog.api import views
 
 urlpatterns = [
-    url(r'^$', views.PostListView.as_view(), name='post_list_api'),
+    url(r'^', views.PostListView.as_view(), name='post_list_api'),
     # url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     # url(r'^post/new/$',views.post_new,name='post_new'),
     # url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
